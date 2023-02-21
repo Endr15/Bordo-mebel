81a648b0760bb57b47ae4256c1876916aaa3a83d
Endr15 <shomkin.andrew@gmail.com>
2023-02-21 23:11:51 +0300

    jq

--- js/common.js
+++ js/common.js
@@ -15,27 +15,6 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
-	MicroModal.init({
-		openTrigger: 'data-custom-open',
-		closeTrigger: 'data-custom-close',
-		disableScroll: true,
-		disableFocus: true,
-		awaitOpenAnimation: true,
-		awaitCloseAnimation: true,
-	})
-	$('[data-custom-open]').click(function () {
-		$('.modal [name=form]').val($(this).data('form'))
-	})
-	$('[data-custom-close]').click(function () {
-		$('.modal [name=form]').val('')
-	})
-
-	/* $('.home-slider__heading').each(function() {
-		let text  = $(this).text().split(' '),
-				first = text.shift()
-		$(this).html(`${first} <br><span>${text.join(' ')}</span>`)
-	}) */
-
 	// Menu
 
 	function toggleMenu() {
@@ -56,6 +35,27 @@ document.addEventListener('DOMContentLoaded', function () {
 		if ($(e.target).closest('.menu-container').length) return
 		closeMenu()
 	})
+
+	MicroModal.init({
+		openTrigger: 'data-custom-open',
+		closeTrigger: 'data-custom-close',
+		disableScroll: true,
+		disableFocus: true,
+		awaitOpenAnimation: true,
+		awaitCloseAnimation: true,
+	})
+	$('[data-custom-open]').click(function () {
+		$('.modal [name=form]').val($(this).data('form'))
+	})
+	$('[data-custom-close]').click(function () {
+		$('.modal [name=form]').val('')
+	})
+
+	/* $('.home-slider__heading').each(function() {
+		let text  = $(this).text().split(' '),
+				first = text.shift()
+		$(this).html(`${first} <br><span>${text.join(' ')}</span>`)
+	}) */
 
 	// Swiper Sliders
 
